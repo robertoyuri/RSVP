f3bfe16a638713c21c09ff75c252e9ae3ff45c7d
Roberto Franco <roberto.yuri.franco@gmail.com>
2026-06-11 20:32:00 -0300

    Initial settings

--- RSVP/RSVP/settings.py
+++ RSVP/RSVP/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'RSVP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
