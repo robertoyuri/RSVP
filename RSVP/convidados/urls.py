a085955465462148a2f3f1265e9883bc68904f83
Roberto Franco <roberto.yuri.franco@gmail.com>
2026-06-15 20:15:18 -0300

    cadastro convidado

--- RSVP/convidados/urls.py
+++ RSVP/convidados/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, cadastro
+
+app_name = 'convidados'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('/cadastro/', cadastro, name='cadastro'),
 ]
--- RSVP/convidados/views.py
+++ RSVP/convidados/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def home(request):
     pais = 'Gana'
     return render(request, 'home.html', {'pais':pais})
+
+def cadastro(request):
+    return render(request, 'convidados/cadastro.html')
